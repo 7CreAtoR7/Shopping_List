8b127bf79cd74e386355a3c08f0751472e6fae10
Ilya Lisitskiy <ilyalisi7skiy@gmail.com>
2022-12-10 01:08:24 +0300

    adding MainViewModel in presentation layer and changing UseCases for automatic list (LiveData) update

--- app/src/main/java/ru/shop/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/ru/shop/shoppinglist/presentation/MainActivity.kt
@@ -1,12 +1,28 @@
 package ru.shop.shoppinglist.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import ru.shop.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.shopList.observe(this) {
+            Log.d("MainActivityTest", it.toString())
+            if (count == 0) {
+                count++ // единожды обновляем статус у первого элемента и сохраняем в LiveData
+                // при повороте экрана меняется статус уже у второго элемента и т.д.
+                val item = it[0]
+                viewModel.changeEnableState(item)
+            }
+        }
     }
 }

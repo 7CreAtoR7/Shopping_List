87030967f4b556ee19d7745f236e074c8a29eb34
Ilya Lisitskiy <ilyalisi7skiy@gmail.com>
2022-12-11 12:39:58 +0300

    adding RecyclerView with both viewTypes and set viewholder's pool size

--- app/src/main/java/ru/shop/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/ru/shop/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -4,19 +4,21 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import ru.shop.shoppinglist.domain.ShopItem
 import ru.shop.shoppinglist.domain.ShopListRepository
+import java.util.TreeSet
+import kotlin.random.Random
 
 object ShopListRepositoryImpl : ShopListRepository {
     // синглтон - где бы мы не обратились к объекту, это будет один и тот же экземпляр во
     // избежании ситуации, когда на первом экране один репозиторий, а на втором - другой репозиторий
 
     private val shopListLiveData = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>(Comparator { o1, o2 -> o1.id.compareTo(o2.id)})
 
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until 10) {
-            val item = ShopItem("Name $i", i, true)
+        for (i in 0 until 1000) {
+            val item = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
